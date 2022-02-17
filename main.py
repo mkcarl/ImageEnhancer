d1ef03f1ba0d71c5e710001c81274697f010a132
mkcarl <munkye29@gmail.com>
2022-02-18 00:50:31 +0800

    done house img

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import PySimpleGUI as sg
-
+from PIL import Image
 
 main_menu_layout = [
-    [sg.Text("Image Enhaner - GROUP x")],
+    [sg.Text("Image Enhaner - GROUP x", font=("Arial", 24, "normal"))],
     [sg.Text("Click on any image below :")],
     [
-        sg.Button("", image_filename="images_original/1-1.png", key="img1"),
-        sg.Button("", image_filename="images_original/001.jpg", key="img2"),
-        sg.Button("", image_filename="images_original/3-1.jpg", key="img3"),
-        sg.Button("", image_filename="images_original/011.jpg", key="img4"),
-        sg.Button("", image_filename="images_original/a.png", key="img5")
+        sg.Button("", image_filename="images_png/0.png", key="img1", image_subsample=3),
+        sg.Button("", image_filename="images_png/1.png", key="img2", image_subsample=3),
+        sg.Button("", image_filename="images_png/2.png", key="img3", image_subsample=3),
+        sg.Button("", image_filename="images_png/3.png", key="img4", image_subsample=10),
+        sg.Button("", image_filename="images_png/4.png", key="img5", image_subsample=2)
     ]
 ]
 
 
 # Create the Window
-window = sg.Window('Window Title', main_menu_layout)
+window = sg.Window('Image enhancer - main menu', main_menu_layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
